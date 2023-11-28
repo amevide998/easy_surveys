806eab745a98e47792ac6623b659d02c446fce38
amevide998 <hdsvidi@gmail.com>
2023-11-28 16:11:06 +0700

    remove this, use router directly

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -15,7 +15,7 @@
                                     :to="item.to"
                                     active-class="bg-gray-900 text-white"
                                     :class="[
-                                        this.$route.name === item.to.name
+                                        $route.name === item.to.name
                                     ? ''
                                     : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
                                     >{{ item.name }}
@@ -73,7 +73,7 @@
                         :to="item.to"
                         active-class="bg-gray-900 text-white"
                         :class="[
-                            this.$route.name === item.to.name
+                            $route.name === item.to.name
                             ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
                     >{{ item.name }}</router-link>
                 </div>
