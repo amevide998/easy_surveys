909024b70b7723a4e29cad0fcb302eb375bc5fdf
amevide998 <hdsvidi@gmail.com>
2023-11-28 23:31:08 +0700

    add register function

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -5,26 +5,60 @@
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" action="#" method="POST">
+            <form class="space-y-6" @submit="register">
                 <div>
-                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Fullname</label>
+                    <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Fullname</label>
                     <div class="mt-2">
-                        <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <input id="name"
+                               name="name"
+                               type="text"
+                               autocomplete="email" required=""
+                               v-model="user.name"
+                               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
                 <div>
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                     <div class="mt-2">
-                        <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <input id="email"
+                               name="email"
+                               type="email"
+                               autocomplete="email"
+                               required=""
+                               v-model="user.email"
+                               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
-
                 <div>
                     <div class="flex items-center justify-between">
                         <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     </div>
+                    <div class="mt-2 relative">
+                        <input id="password"
+                               name="password"
+                               type="password"
+                               autocomplete="current-password"
+                               v-model="user.password"
+                               required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        <div class="absolute right-3 top-1">
+<!--                            eye-->
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="flex items-center justify-between">
+                        <label for="password_confirmation" class="block text-sm font-medium leading-6 text-gray-900">
+                            Password Confirmation
+                        </label>
+                    </div>
                     <div class="mt-2">
-                        <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <input id="password_confirmation"
+                               name="password_confirmation"
+                               type="password"
+                               autocomplete="current-password"
+                               required=""
+                               v-model="user.password_confirmation"
+                               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
 
@@ -44,9 +78,28 @@
         </div>
 </template>
 
-<script>
-export default {
-    name: "Register"
+<script setup>
+import store from "../store/index.js"
+import {useRoute, useRouter} from "vue-router"
+
+const router = useRouter()
+
+const user = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+}
+
+function register(ev){
+    ev.preventDefault()
+    store
+        .dispatch('register', user)
+        .then(() => {
+            router.push({
+                name: 'Dashboard'
+            })
+        })
 }
 </script>
 
