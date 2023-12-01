ce3b5246f887f8082a5d799c6c162afc2274b159
amevide998 <hdsvidi@gmail.com>
2023-12-02 02:59:18 +0700

    add login auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rules\Password;
 
@@ -34,6 +35,33 @@ class AuthController extends Controller
             'token'=> $token
         ]);
 
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'=> 'required|email|string|exists:users,email',
+            'password'=> 'required',
+            'remember'=> 'boolean'
+        ]);
+
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if(!Auth::attempt($credentials, $remember))
+        {
+            return response([
+                'error'=> 'unauthorized'
+            ], 422);
+        }
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user'=> $user,
+            'token'=> $token
+        ]);
     }
 
 }
