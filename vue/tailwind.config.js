d7c4a2ddd79ae06748060c5e6442a0fac9d2a5e1
amevide998 <hdsvidi@gmail.com>
2023-12-11 18:55:51 +0700

    add animation fade in down

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{vue, js, ts, jsx, tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-0.75rem)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0rem)'
+          }
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-in-out both'
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
