1507d9964cc82f52173f75dedb9f39c406e4ebe0
amevide998 <hdsvidi@gmail.com>
2023-12-11 19:45:49 +0700

    implement pagination

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -11,15 +11,40 @@
                 </router-link>
             </div>
             <div v-if="surveys.loading" class="flex justify-center">Loading ...</div>
-            <div v-else class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
-                <SurveyListItem
-                    v-for="(survey, index) in surveys.data"
-                    class='opacity-0 animate-fade-in-down'
-                    :style="{animationDelay: `${index * 0.1}s`}"
-                    :key="survey.id"
-                    :survey="survey"
-                    @delete="deleteSurvey(survey)"
-                />
+            <div v-else>
+                <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+                    <SurveyListItem
+                        v-for="(survey, index) in surveys.data"
+                        class='opacity-0 animate-fade-in-down'
+                        :style="{animationDelay: `${index * 0.1}s`}"
+                        :key="survey.id"
+                        :survey="survey"
+                        @delete="deleteSurvey(survey)"
+                    />
+                </div>
+
+                <div class="flex justify-center mt-5">
+                    <nav class="relative z-0 inline-flex justify-center rounded-md shadow-md px-2 -space-x-px"
+                         aria-label="Pagination">
+                        <a
+                            v-for="(link, i) in surveys.links"
+                            :key="i"
+                            :disabled="!link.url"
+                            v-html="link.label"
+                            href="#"
+                            @click="getForPage($event,link)"
+                            aria-current="page"
+                            class="relative inline-flex items-center px-4 py-2 text-sm font-medium whitespace-nowrap"
+                            :class="[
+                                link.active
+                                ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                                : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+                            ]"
+                        >
+                        </a>
+                    </nav>
+
+                </div>
             </div>
         </template>
     </PageComponent>
@@ -40,6 +65,14 @@ async function deleteSurvey(survey){
         await store.dispatch('deleteSurvey', survey.id)
         await store.dispatch('getSurveys')
     }
+}
+
+async function getForPage(ev, link){
+    ev.preventDefault()
+    if(link.url === null || link.active){
+        return
+    }
+    await store.dispatch('getSurveys', {url: link.url})
 }
 
 </script>
