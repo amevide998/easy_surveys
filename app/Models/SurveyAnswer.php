5ae96f4fb358693ec58d9a9e954c019847fdd446
amevide998 <hdsvidi@gmail.com>
2023-12-11 23:34:40 +0700

    add fillable field and const time

--- app/Models/SurveyAnswer.php
+++ app/Models/SurveyAnswer.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class SurveyAnswer extends Model
 {
     use HasFactory;
+    protected $fillable = ['survey_id', 'start_date', 'end_date'];
+
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
 }
