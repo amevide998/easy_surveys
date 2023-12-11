aa13eaa001c5b4b98f3f2bf505741d34bcbb7e06
amevide998 <hdsvidi@gmail.com>
2023-12-11 18:32:48 +0700

    initial notification template view

--- vue/src/components/Notification.vue
+++ vue/src/components/Notification.vue
@@ -3,7 +3,6 @@ import {computed, ref} from "vue";
 import {useStore} from "vuex";
 
 const store = useStore();
-
 const notification = computed(() => store.state.notification);
 
 
@@ -11,6 +10,13 @@ const notification = computed(() => store.state.notification);
 </script>
 
 <template>
+    <div
+        v-if="notification.show"
+        class="fixed w-[300px] left-4 bottom-4 py-2 px-4 text-white animate-fade-in-down"
+        :class="[notification.type === 'success' ? 'bg-emerald-500' : 'bg-red-500' ]"
+    >
+        {{notification.message}}
+    </div>
 
 </template>
 
