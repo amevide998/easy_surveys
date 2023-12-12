f940520e72eb523268065ec1247026e8c1c51b00
amevide998 <hdsvidi@gmail.com>
2023-12-12 09:31:46 +0700

    implement alert for error message

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -6,12 +6,12 @@
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" @submit="login">
-                <div v-if = "errorMsg" class="py-3 px-5 bg-red-500 text-white rounded">
+                <Alert>
                     {{errorMsg}}
                     <span @click="errorMsg = ''">
                         X
                     </span>
-                </div>
+                </Alert>
                 <div>
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                     <div class="mt-2">
@@ -56,8 +56,21 @@
                         </label>
                     </div>
                 </div>
-                <div>
-                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                <div >
+                    <button :disabled="loading"
+                            type="submit"
+                            class="flex w-full justify-center items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <svg v-if="loading"
+                             class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                             xmlns="http://www.w3.org/2000/svg"
+                             fill="none"
+                             viewBox="0 0 24 24"
+                        >
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        Sign in
+                    </button>
                 </div>
             </form>
 
@@ -75,6 +88,7 @@
 import store from "../store/index.js"
 import {useRouter} from "vue-router"
 import {ref} from "vue";
+import Alert from "../components/Alert.vue";
 
 const user = {
     email : '',
@@ -83,17 +97,21 @@ const user = {
 }
 
 let errorMsg = ref('')
+let loading = ref(false)
 
 const router = useRouter()
 
 async function login(ev){
+    loading.value = true
     ev.preventDefault()
     try {
         await store.dispatch('login', user)
         await router.push({
             name: 'Dashboard'
         })
+        loading.value = false
     }catch (err){
+        loading.value = false
         errorMsg = await err.response.data.error
     }
 
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -2,8 +2,34 @@
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
             <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register for free</h2>
-        </div>
+            <Alert v-if="Object.keys(errors).length"
+                   class="flex-col items-stretch text-sm"
+            >
+                <div class="flex justify-between">
+                    <div>
+                        <div
+                            v-for="(field, i) in Object.keys(errors)"
+                            :key="i"
+                        >
+                            <div
+                                v-for="(error, i) of errors[field] || []"
+                                :key="i"
+                            >
+                                * {{error}}
+                            </div>
+                        </div>
+                    </div>
+                    <span
+                        v-if="Object.keys(errors).length"
+                        @click="errors = ''"
+                        class="flex justify-center items-center font-bold cursor-pointer hover:text-red-500 hover:bg-white w-6 h-6 rounded-full"
+                    > X
+                    </span>
 
+                </div>
+            </Alert>
+
+        </div>
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" @submit="register">
                 <div>
@@ -63,7 +89,18 @@
                 </div>
 
                 <div>
-                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    <button :disabled="loading"
+                            type="submit"
+                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <svg v-if="loading"
+                             class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                             xmlns="http://www.w3.org/2000/svg"
+                             fill="none"
+                             viewBox="0 0 24 24"
+                        >
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
                         Register
                     </button>
                 </div>
@@ -81,6 +118,8 @@
 <script setup>
 import store from "../store/index.js"
 import {useRoute, useRouter} from "vue-router"
+import {ref} from "vue";
+import Alert from "../components/Alert.vue";
 
 const router = useRouter()
 
@@ -91,12 +130,25 @@ const user = {
     password_confirmation: ''
 }
 
+let errors = ref({})
+let loading = ref(false)
+
 async function register(ev){
-    ev.preventDefault()
-    await store.dispatch('register', user)
-    await router.push({
-        name: 'Dashboard'
-    })
+    try {
+        loading.value = true
+        ev.preventDefault()
+        await store.dispatch('register', user)
+        await router.push({
+            name: 'Dashboard'
+        })
+        loading.value = false
+    }catch (err){
+        if(err.response.status === 422){
+            loading.value = false
+            errors.value = err.response.data.errors
+        }
+    }
+
 }
 </script>
 
