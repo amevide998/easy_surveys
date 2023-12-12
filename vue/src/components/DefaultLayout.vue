03330f740161ef99ed046700a0798803678259e2
amevide998 <hdsvidi@gmail.com>
2023-12-12 19:00:30 +0700

    update style

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="min-h-full">
-        <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+        <Disclosure as="nav" class="bg-[#1B4242]" v-slot="{ open }">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
                         <div class="flex-shrink-0">
-                            <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                            <img class="h-10" src="/simpleq-high-resolution-logo-white-transparent.png" alt="simpleQ logo" />
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
@@ -13,11 +13,11 @@
                                     v-for="item in navigation"
                                     :key="item.name"
                                     :to="item.to"
-                                    active-class="bg-gray-900 text-white"
+                                    active-class="bg-[#092635] text-white"
                                     :class="[
                                         $route.name === item.to.name
                                     ? ''
-                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
+                                    : 'text-gray-300 hover:bg-[#092635] hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
                                     >{{ item.name }}
                                 </router-link>
                             </div>
@@ -25,7 +25,7 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-4 flex items-center md:ml-6">
-                            <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <button type="button" class="relative rounded-full bg-[#1B4242] p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                 <span class="absolute -inset-1.5" />
                                 <span class="sr-only">View notifications</span>
                                 <BellIcon class="h-6 w-6" aria-hidden="true" />
@@ -34,10 +34,15 @@
                             <!-- Profile dropdown -->
                             <Menu as="div" class="relative ml-3">
                                 <div>
-                                    <MenuButton class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                    <MenuButton class="relative flex max-w-xs items-center rounded-full bg-[#1B4242] text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                         <span class="absolute -inset-1.5" />
                                         <span class="sr-only">Open user menu</span>
-                                        <img class="h-8 w-8 rounded-full" :src="user.data.imageUrl" alt="" />
+                                        <div class="border border-[#092635] rounded-full ">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-8 h-8">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                            </svg>
+                                        </div>
+<!--                                        <img class="h-8 w-8 rounded-full" :src="user.data.image_url" alt="profile picture" />-->
                                     </MenuButton>
                                 </div>
                                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
